df80021c5b1f31045b5702dcec64fd9538e8c6b1
Broc <127725696+dwaynethebroc@users.noreply.github.com>
2025-03-13 17:39:05 +1000

    commented out console.logs, moving on to board game logic

--- battleship.js
+++ battleship.js
@@ -18,30 +18,30 @@ class Ship {
         const alreadyHit = this.targetedSquares.includes(square);
 
         if (alreadyHit) {
-            this.message(
-                `You have already selected ${square} before, choose another tile to hit`,
-            );
+            // this.message(
+            //     `You have already selected ${square} before, choose another tile to hit`,
+            // );
 
             return;
         } else if (hitShip && !alreadyHit) {
             this.damage++;
             this.targetedSquares.push(square);
 
-            this.message(
-                `Battleship is hit: ${this.type} \n Damage: ${this.damage} \n Squares hit: ${this.targetedSquares}`,
-            );
+            // this.message(
+            //     `Battleship is hit: ${this.type} \n Damage: ${this.damage} \n Squares hit: ${this.targetedSquares}`,
+            // );
 
             return true;
         } else {
-            this.message(`MISS: no Battleship was hit`);
+            // this.message(`MISS: no Battleship was hit`);
             return false;
         }
     }
 
     isSunk() {
         if (this.targetedSquares.length === this.placement.length) {
-            this.message(this.targetedSquares);
-            this.message(this.placement);
+            // this.message(this.targetedSquares);
+            // this.message(this.placement);
 
             this.sunk = true;
             return true;
