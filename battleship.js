7172a95ca2347377705ecd0a316c351fb8e159a4
Broc <127725696+dwaynethebroc@users.noreply.github.com>
2025-03-13 17:11:14 +1000

    added more properties and test

--- battleship.js
+++ battleship.js
@@ -1,11 +1,37 @@
 export { Ship };
 
 class Ship {
-    constructor(length, placement) {
-        this.hit = 0;
+    constructor(length, placement, type) {
+        //properties
+        this.damage = 0;
+        this.sunk = false;
+        this.targetedSquares = [];
 
+        //public properties
         this.length = length;
         this.placement = placement;
-        this.sunk = false;
+        this.type = type;
+    }
+
+    hit(square) {
+        const hitShip = this.placement.includes(square);
+
+        if (hitShip) {
+            this.damage++;
+            this.targetedSquares.push(square);
+
+            this.message(
+                `Battleship is hit: ${this.type} \n Damage: ${this.damage} \n Squares hit: ${this.targetedSquares}`,
+            );
+
+            return true;
+        } else {
+            this.message(`MISS: no Battleship was hit`);
+            return false;
+        }
+    }
+
+    message(message) {
+        console.log(message);
     }
 }
--- battleship.test.js
+++ battleship.test.js
@@ -1,17 +1,25 @@
 import { Ship } from "./battleship.js";
 
 test("Ship exists", () => {
-    const testDestroyer = new Ship(5, ["A2", "A3", "A4", "A5", "A6"]);
+    const testDestroyer = new Ship(
+        5,
+        ["A2", "A3", "A4", "A5", "A6"],
+        "destroyer",
+    );
 
     expect(testDestroyer).toEqual({
-        hit: 0,
+        damage: 0,
         length: 5,
         placement: ["A2", "A3", "A4", "A5", "A6"],
         sunk: false,
+        targetedSquares: [],
+        type: "destroyer",
     });
     expect(testDestroyer.length).toBe(5);
     expect(testDestroyer.placement).toEqual(["A2", "A3", "A4", "A5", "A6"]);
     expect(testDestroyer.placement).toHaveLength(5);
     expect(testDestroyer.sunk).toBe(false);
-    expect(testDestroyer.hit).toBe(0);
+    expect(testDestroyer.damage).toBe(0);
+    expect(testDestroyer.type).toBe("destroyer");
+    expect(testDestroyer.hit("A2")).toBe(true);
 });
